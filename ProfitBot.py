9cf3692a18d90b5318554003a74d2d491d727f03
karimboulahia <72663780+karimboulahia@users.noreply.github.com>
2025-03-04 21:39:13 +0100

    Update ProfitBot.py

--- ProfitBot.py
+++ ProfitBot.py
@@ -1,5 +1,6 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
+
 
 # Replace this with your Telegram Bot Token
 TOKEN = "8070911062:AAGRIeT4hZbc8MMqZyTyuk7DfuNo--KAVSY"
